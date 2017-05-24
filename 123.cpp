b9ebc0f7b820afefdd1198f1acfb9bdc9af2a4ee
Suryaveer <1998sveer@gmail.com>
2017-05-24 16:27:12 +0530

    Add files via upload

---
+++ 123.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	long long res=0,q=5,r=1;
+	for(int i=0;i<1000000;i++){
+		int gg;
+		cin>>gg;
+		res  = res + gg;
+	}
+	cout<<res<<endl;
+	cout<<q<<endl;
+	return 0;
+}
